10f0e9bb444ff900e759b9862bcd4032009858bf
JDiaz <jdiaz_007@outlook.com>
2023-10-01 20:18:51 -0700

    started useEffect and added useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Gallery from "./Components/Gallery.jsx"
-import SearchBar from "./Components/SearchBar.jsx"
+import { useEffect, useState } from 'react';
+import Gallery from "./Components/Gallery.jsx";
+import SearchBar from "./Components/SearchBar.jsx";
 
 function App() {
+  let [search, setSearch] = useState("")
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState("Search for Music!")
   return (
     <div className="App">
       <SearchBar />
+      {message}
       <Gallery />
     </div>
   );
