ab0810a99f0762ae805a0a04ace28458efb30360
JDiaz <jdiaz_007@outlook.com>
2023-10-01 20:28:10 -0700

    added a form and useState to SearchBar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,5 +1,12 @@
+import { useState } from 'react'
+
 export default function SearchBar() {
+    let [searchTerm, setSearchTerm] = useState('')
+
     return (
-        <h1>SearchBar</h1>
+        <form>
+            <input type="text" placeholder='Enter a search term' />
+            <input type="submit" />
+        </form>
     )
 }
