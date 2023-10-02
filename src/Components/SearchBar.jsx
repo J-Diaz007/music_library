9eab15b623924e29e41a29f375f5a0753f6ae9e1
JDiaz <jdiaz_007@outlook.com>
2023-10-02 11:22:24 -0700

    removed the onChange property from text input and added ref variable to the text input. Imported useContext and SearchContext

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { SearchContext } from '../Context/SearchContext.js'
+import { useContext } from 'react'
 
 export default function SearchBar() {
     return (
         <form onSubmit={e => handleSearch(e, searchTerm)}>
-            <input type="text" placeholder='Enter a search term' onChange={e => setSearchTerm(e.target.value)} />
+            <input type="text" placeholder='Enter a search term' ref={term}/>
             <input type="submit" />
         </form>
     )
