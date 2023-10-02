69cdcacd1e7e54aae15f253a769204e6d867a112
JDiaz <jdiaz_007@outlook.com>
2023-10-02 11:12:32 -0700

    declared searchInput and assigned the SearchContext new values by assigning handleSearch to the Context's handleSearch field and input ref as the term

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from "./Components/Gallery.jsx"
 import SearchBar from "./Components/SearchBar.jsx"
-import { DataContext } from "./Context/DataContext"
+import { DataContext } from "./Context/DataContext.js"
+import { SearchContext } from "./Context/SearchContext.js"
+
 
 const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -10,6 +12,7 @@ function App() {
   let [search, setSearch] = useState("")
   let [data, setData] = useState([])
   let [message, setMessage] = useState("Search for Music!")
+  let searchInput = useRef('')
 
   useEffect(() => {
     if(search) {
@@ -34,7 +37,12 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
+      <SearchContext.Provider value={{
+          term: searchInput,
+          handleSearch: handleSearch
+      }}>
+        <SearchBar />
+      </SearchContext.Provider>
       {message}
       <DataContext.Provider value={data} >
         <Gallery />
--- src/Context/SearchContext.js
+++ src/Context/SearchContext.js
@@ -4,7 +4,7 @@ export const SearchContext = createContext({
     term: '',
     handleSearch: () => {}
 })
-matchMedia
+
 
 
 
