b0c3d9631e403747964274b2fd8ff4c7b499c6f9
JDiaz <jdiaz_007@outlook.com>
2023-10-01 21:18:29 -0700

    fetch data from itunes API

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   let [search, setSearch] = useState("")
   let [data, setData] = useState([])
   let [message, setMessage] = useState("Search for Music!")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`
+      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+      const resData = await response.json()
+      setData(resData)
+    }
+  }, [])
+
   return (
     <div className="App">
       <SearchBar />
