32416da911ab948555d38ea4c28b6b9e819f8e8f
JDiaz <jdiaz_007@outlook.com>
2023-10-01 21:57:35 -0700

    reshaped API call to feed custom data as the search query

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import { useEffect, useState } from 'react';
 import Gallery from "./Components/Gallery.jsx";
 import SearchBar from "./Components/SearchBar.jsx";
 
+const API_URL = 'https://itunes.apple.com/search?term='
+
 function App() {
   let [search, setSearch] = useState("")
   let [data, setData] = useState([])
   let [message, setMessage] = useState("Search for Music!")
 
   useEffect(() => {
-    const fetchData = async () => {
-      document.title = `${search} Music`
-      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-      const resData = await response.json()
-      if (resData.results.length) {
-        setData(resData.results)
-      } else {
-        setMessage('Not found 😞')
-      }      
+    if(search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+        const response = await fetch('API_URL + search')
+        const resData = await response.json()
+        if (resData.results.length) {
+          setData(resData.results)
+        } else {
+          setMessage('Not found 😞')
+        }      
+      }
+      fetchData()
     }
-  }, [])
+  }, [search])
 
   const handleSearch = (e, term) => {
     e.preventDefault()
