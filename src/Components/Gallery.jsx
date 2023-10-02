caaeac791c837f9bad258e44d7024ccbe32df2da
JDiaz <jdiaz_007@outlook.com>
2023-10-01 22:09:31 -0700

    added different views in GalleryItem.jsx (detailed and simple)

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,6 +1,7 @@
 import GalleryItem from "./GalleryItem"
 
 export default function Gallery(props) {
+    
     const display = props.data.map((item, index) =>
      <GalleryItem item={item} index={index} />)
 
--- src/Components/GalleryItem.jsx
+++ src/Components/GalleryItem.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-export default function GalleryItem() {
-    let [showDetails, setShowDetails] = useState(false)
+export default function GalleryItem(props) {
+    let [view, setView] = useState(false)
+
+    const simpleView = () => {
+        return (
+            <div>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
+        )
+    }
+
+    const detailView = () => {
+        return (
+            <div>
+                <h2>{props.item.trackName}</h2>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
 
     return (
-        <div onClick={() => setShowDetails(!showDetails)} 
+        <div onClick={() => setView(!view)}
         style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
+        
+            {/* This simple ternary shows the simple view when 'view' is false! */}
+            {view ? detailView() : simpleView()}
+
         </div>
     )
+
 }
