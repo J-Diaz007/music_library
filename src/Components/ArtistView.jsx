8720f23beb6993f3803916d2595f8655d9ab810c
JDiaz <jdiaz_007@outlook.com>
2023-10-04 22:40:08 -0700

    added AlbumView and ArtistView components. Exported new component functions

---
+++ src/Components/ArtistView.jsx
@@ -0,0 +1,13 @@
+// This component will be making separate API calls from the app component to serve specific data about our artist
+import { useState, useEffect } from 'react'
+
+export default function ArtistView() {
+    const [ artistData, setArtistData] = useState([])
+
+    return(
+        <div>
+            <p>Artist Data goes here!</p>
+        </div>
+    )
+}
+
